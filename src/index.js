9d900f738daa8322387c17cabb77b45dd6f2c0f0
janinebknr <87192949+janinebknr@users.noreply.github.com>
2022-04-02 17:43:25 +0100

    correcting the static server

--- src/index.js
+++ src/index.js
@@ -21,14 +21,14 @@ app.use("/content", express.static(path.join(__dirname, "public")));
 app.get("/", (_, res) => {
   res.sendFile("./public/index.html", { root: __dirname });
 
-  res.status(501).end();
+  // res.status(501).end();
 });
 
 app.get("/todos", (_, res) => {
   res.header("Content-Type", "application/json");
   res.sendFile(todoFilePath, { root: __dirname });
 
-  res.status(501).end();
+  // res.status(501).end();
 });
 
 //Add GET request with path '/todos/overdue'
